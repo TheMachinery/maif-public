241bc69da79931b4bc75ba61e55fa5a131e1645a
Amaury Laroze <amaury@themachinery.fr>
2018-05-03 10:24:02 +0200

    deploy

--- public/app.js
+++ public/app.js
@@ -3784,7 +3784,7 @@ module.exports = [{"name":"Données contractuelles de propriétés","location":"
 Object.defineProperty(exports, "__esModule", {
     value: true
 });
-var VERSION = exports.VERSION = '1.0.161';
+var VERSION = exports.VERSION = '1.0.162';
 
 var DOCTYPE_EQUIPMENTS = exports.DOCTYPE_EQUIPMENTS = 'com.empreinte.equipment';
 var DOCTYPE_WORKS = exports.DOCTYPE_WORKS = 'com.empreinte.work';
@@ -11370,7 +11370,7 @@ var Footer = function (_Component) {
                         'span',
                         null,
                         'V',
-                        '1.0.161'
+                        '1.0.162'
                     )
                 )
             );
@@ -24775,7 +24775,7 @@ module.exports = [{"name":"Données contractuelles de propriétés","location":"
 Object.defineProperty(exports, "__esModule", {
     value: true
 });
-var VERSION = exports.VERSION = '1.0.161';
+var VERSION = exports.VERSION = '1.0.162';
 
 var DOCTYPE_EQUIPMENTS = exports.DOCTYPE_EQUIPMENTS = 'com.empreinte.equipment';
 var DOCTYPE_WORKS = exports.DOCTYPE_WORKS = 'com.empreinte.work';
@@ -26320,7 +26320,7 @@ function _interopRequireDefault(obj) { return obj && obj.__esModule ? obj : { de
 var root = document.querySelector('[role=application]');
 var data = root.dataset;
 
-console.log('Current Version:', '1.0.161');
+console.log('Current Version:', '1.0.162');
 
 document.addEventListener('DOMContentLoaded', function () {
 
