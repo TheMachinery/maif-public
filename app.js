273bdc236dfa907d6b0ccd4e3bda2da7261ae3ba
Amaury Laroze <amaury@themachinery.fr>
2018-05-11 10:16:42 +0200

    deploy

--- app.js
+++ app.js
@@ -1083,13 +1083,13 @@ var _Impot = __webpack_require__("./src/containers/Impot.js");
 
 var _Impot2 = _interopRequireDefault(_Impot);
 
-var _scroll = __webpack_require__("./src/tools/scroll.jsx");
-
-var _scroll2 = _interopRequireDefault(_scroll);
-
 function _interopRequireDefault(obj) { return obj && obj.__esModule ? obj : { default: obj }; }
 
 var history = (0, _history.createHashHistory)();
+
+history.listen(function () {
+  return window.scrollTo(0, 0);
+});
 
 // app component
 var App = function App(props) {
@@ -1110,9 +1110,7 @@ var App = function App(props) {
   // render
   return _react2.default.createElement(
     _reactRouter.Router,
-    { onUpdate: function onUpdate() {
-        return document.body.scrollTop = 0;
-      }, history: history },
+    { history: history },
     _react2.default.createElement(
       'div',
       { className: _App2.default.app },
@@ -1120,7 +1118,6 @@ var App = function App(props) {
         'div',
         { className: _App2.default.container },
         _react2.default.createElement(_Navigation2.default, null),
-        _react2.default.createElement(_scroll2.default, null),
         _react2.default.createElement(
           'div',
           { className: _App2.default.page },
@@ -33592,58 +33589,6 @@ function string_to_slug(str) {
 
     return str;
 }
-
-/***/ }),
-
-/***/ "./src/tools/scroll.jsx":
-/***/ (function(module, exports, __webpack_require__) {
-
-"use strict";
-
-
-Object.defineProperty(exports, "__esModule", {
-  value: true
-});
-
-var _createClass = function () { function defineProperties(target, props) { for (var i = 0; i < props.length; i++) { var descriptor = props[i]; descriptor.enumerable = descriptor.enumerable || false; descriptor.configurable = true; if ("value" in descriptor) descriptor.writable = true; Object.defineProperty(target, descriptor.key, descriptor); } } return function (Constructor, protoProps, staticProps) { if (protoProps) defineProperties(Constructor.prototype, protoProps); if (staticProps) defineProperties(Constructor, staticProps); return Constructor; }; }();
-
-var _react = __webpack_require__("./node_modules/react/react.js");
-
-var _react2 = _interopRequireDefault(_react);
-
-var _reactRouter = __webpack_require__("./node_modules/react-router/es/index.js");
-
-function _interopRequireDefault(obj) { return obj && obj.__esModule ? obj : { default: obj }; }
-
-function _classCallCheck(instance, Constructor) { if (!(instance instanceof Constructor)) { throw new TypeError("Cannot call a class as a function"); } }
-
-function _possibleConstructorReturn(self, call) { if (!self) { throw new ReferenceError("this hasn't been initialised - super() hasn't been called"); } return call && (typeof call === "object" || typeof call === "function") ? call : self; }
-
-function _inherits(subClass, superClass) { if (typeof superClass !== "function" && superClass !== null) { throw new TypeError("Super expression must either be null or a function, not " + typeof superClass); } subClass.prototype = Object.create(superClass && superClass.prototype, { constructor: { value: subClass, enumerable: false, writable: true, configurable: true } }); if (superClass) Object.setPrototypeOf ? Object.setPrototypeOf(subClass, superClass) : subClass.__proto__ = superClass; }
-
-var ScrollToTop = function (_React$Component) {
-  _inherits(ScrollToTop, _React$Component);
-
-  function ScrollToTop() {
-    _classCallCheck(this, ScrollToTop);
-
-    return _possibleConstructorReturn(this, (ScrollToTop.__proto__ || Object.getPrototypeOf(ScrollToTop)).apply(this, arguments));
-  }
-
-  _createClass(ScrollToTop, [{
-    key: "componentDidUpdate",
-    value: function componentDidUpdate() {}
-  }, {
-    key: "render",
-    value: function render() {
-      return null;
-    }
-  }]);
-
-  return ScrollToTop;
-}(_react2.default.Component);
-
-exports.default = (0, _reactRouter.withRouter)(ScrollToTop);
 
 /***/ }),
 
